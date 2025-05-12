cbb294c3dc13f1f23467575e2a2ba3a57539dc36
Panuwat Sangsuriya <panuwat.s@arise.tech>
2025-05-12 16:22:46 +0700

    change next config and add hero ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { heroui } from "@heroui/react";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [heroui()],
 } satisfies Config;
