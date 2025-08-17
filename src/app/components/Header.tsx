7ad4ceb47b9784ea032cbae0f34e65c68e7d00f7
Panuwat Sangsuriya <panuwat.s@arise.tech>
2025-08-17 21:35:58 +0700

    update padding

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -26,7 +26,7 @@ type Props = {
 
 function Header({ title, refetch }: Props) {
   return (
-    <div className="flex flex-row justify-between px-4 pt-8 pb-4 w-full">
+    <div className="flex flex-row justify-between px-4 pt-6 pb-4 w-full">
       <p
         className="text-[32px] font-medium text-gray-900"
         onClick={() => refetch()}
